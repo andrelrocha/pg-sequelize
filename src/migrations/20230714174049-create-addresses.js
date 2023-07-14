cddfcfb34baa0e7f5f6378a14152a6ab23b4c957
andrelrocha <andrerocha0911@gmail.com>
2023-07-14 14:45:04 -0300

    feat: criando relação entre table de addreses para users

---
+++ src/migrations/20230714174049-create-addresses.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { on } = require('events');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('addresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type : Sequelize.INTEGER,
+        references : {
+          model : 'users',
+          key : 'id'
+        },
+        onDelete : 'CASCADE',
+        onUpdate : 'CASCADE'
+      },
+      zipcode: {
+        type: Sequelize.STRING
+      },
+      street: {
+        type: Sequelize.STRING
+      },
+      number: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('addresses');
+  }
+};
